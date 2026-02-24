3817c547e7c6fda28777c7e6018e8bfea2c733c0
Симонов Евгений Дмитриевич <jdsimonov2@gmail.com>
2026-02-24 09:34:32 +0300

    feat: add calculator

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Calculator from './Calculator'
+import './style.css'
+
+function App() {
+  return (
+    <>
+      <h1 style={{textAlign: 'center'}}>Калькулятор Vite</h1>
+      <Calculator/>
+    </>
+  )
+}
+
+export default App
